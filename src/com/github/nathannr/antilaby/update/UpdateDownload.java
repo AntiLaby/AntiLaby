0e3cfcaf2a93439b817c35252079d9417ac59778
Nathan <nathan.noerthemann@outlook.de>
2016-11-11 21:43:49 +0100

    Async update checking and async auto update

---
+++ src/com/github/nathannr/antilaby/update/UpdateDownload.java
@@ -0,0 +1,109 @@
+package com.github.nathannr.antilaby.update;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
+import com.github.nathannr.antilaby.main.AntiLaby;
+
+public class UpdateDownload extends Thread {
+
+	boolean updateAvailable;
+	
+	@SuppressWarnings("deprecation")
+	@Override
+	public void run() {
+		//Check for update
+		System.out.println(AntiLaby.getInstance().cprefixinfo + "Checking for updates on spigotmc.org...");
+		try {
+            HttpURLConnection con = (HttpURLConnection) new URL("http://www.spigotmc.org/api/general.php").openConnection();
+            con.setDoOutput(true);
+            con.setRequestMethod("POST");
+            con.getOutputStream().write(("key=98BE0FE67F88AB82B4C197FAF1DC3B69206EFDCC4D3B80FC83A00037510B99B4&resource=" + AntiLaby.getInstance().resource).getBytes("UTF-8"));
+            String version = new BufferedReader(new InputStreamReader(con.getInputStream())).readLine();
+            if (!version.equalsIgnoreCase(AntiLaby.getInstance().getDescription().getVersion())) {
+            	System.out.println(AntiLaby.getInstance().cprefixinfo + "Update found! Version " + version + " is aviable.");
+            	updateAvailable = true;
+            } else {
+            	System.out.println(AntiLaby.getInstance().cprefixinfo + "No update found. You are running the newest version.");
+            	updateAvailable = false;
+            }
+        } catch (Exception ex) {
+        	System.err.println(AntiLaby.getInstance().cprefixerr + "Failed to check for updates on spigotmc.org!");
+    		updateAvailable = false;
+        }
+		//Download and install update if available
+		if(updateAvailable == true) {
+			File aljar = new File("plugins/AntiLaby.jar");
+			if(!aljar.exists()) {
+				System.err.println(AntiLaby.getInstance().cprefixerr + "Auto-update failed!");
+				int i = 5;
+				while(i >= 0) {
+					System.err.println(AntiLaby.getInstance().cprefixerr + "PLEASE RENAME THE ANTILABY PLUGIN BACK TO 'AntiLaby.jar'! OTHERWISE AUTO-UPDATE WON'T WORK! IF YOU DON'T WANT TO USE AUTO-UPDATE DISABLE IT IN THE CONFIG FILE!");
+					i--;
+				}
+			}
+			System.out.println(AntiLaby.getInstance().cprefixinfo + "Downloading update...");
+			URL url;
+			try {
+				
+				url = new URL("http://adf.ly/1f1ZEn");
+				final URLConnection conn = url.openConnection();
+				final InputStream is = new BufferedInputStream(conn.getInputStream());
+				if(is != null) {
+					final OutputStream os = new BufferedOutputStream(new FileOutputStream("plugins/AntiLaby.tmp"));
+					byte[] chunk = new byte[1024];
+					int chunkSize;
+					while ((chunkSize = is.read(chunk)) != -1) {
+						os.write(chunk, 0, chunkSize);
+					}
+					os.close();
+					File newfile = new File("plugins/AntiLaby.tmp");
+					long newlength = newfile.length();
+					if(newlength <= 10000) {
+						newfile.delete();
+						System.err.println(AntiLaby.getInstance().cprefixerr + "Auto-update failed! Update server down? You still have version " + AntiLaby.getInstance().getDescription().getVersion() + ". Please install the newest version manually from https://www.spigotmc.org/resources/" + AntiLaby.getInstance().resource + "/!");
+					} else {
+						System.out.println(AntiLaby.getInstance().cprefixinfo + "Installing update...");
+						
+						final FileInputStream is2 = new FileInputStream(new File("plugins/AntiLaby.tmp"));
+						
+						final OutputStream os2 = new BufferedOutputStream(new FileOutputStream("plugins/AntiLaby.jar"));
+						byte[] chunk2 = new byte[1024];
+						int chunkSize2;
+						while ((chunkSize2 = is2.read(chunk2)) != -1) {
+							os2.write(chunk2, 0, chunkSize2);
+						}
+						is2.close();
+						os2.close();
+						
+						File tmp = new File("plugins/AntiLaby.tmp");
+						tmp.delete();
+						
+						System.out.println(AntiLaby.getInstance().cprefixinfo + "Auto-update complete!");
+					}
+				}
+				
+			} catch (IOException e) {
+				System.err.println(AntiLaby.getInstance().cprefixerr + "Auto-update failed! You still have version " + AntiLaby.getInstance().getDescription().getVersion() + ". Please install the newest version manually from https://www.spigotmc.org/resources/" + AntiLaby.getInstance().resource + "/!");
+			}
+			File tmp = new File("plugins/AntiLaby.tmp");
+			if(tmp.exists()) {
+				tmp.delete();
+			}
+		}
+		
+		this.stop();
+	}
+	
+}
