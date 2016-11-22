3d54afa5b23730a64830f77c0fb5458ffd1344cc
Nathan <nathan.noerthemann@outlook.de>
2016-11-22 20:17:28 +0100

    Added NMS-class (unused at the moment)

---
+++ src/com/github/nathannr/antilaby/utils/NMS.java
@@ -0,0 +1,41 @@
+package com.github.nathannr.antilaby.utils;
+
+import java.lang.reflect.Field;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+public class NMS {
+
+	static String getVersion() {
+		String name = Bukkit.getServer().getClass().getPackage().getName();
+	    String version = name.substring(name.lastIndexOf('.') + 1) + ".";
+	    return version;
+	}
+	
+	public static Class<?> getNMSClass(String className) {
+		String fullName = "net.minecraft.server." + getVersion() + className;
+	    Class<?> clazz = null;
+	    try {
+	    	clazz = Class.forName(fullName);
+	    } catch (Exception e) {
+	    	e.printStackTrace();
+	    }
+	    return clazz;
+	}
+	
+	public static Object getNMSPlayer(Player p) {
+		try {
+			return p.getClass().getMethod("getHandle").invoke(p);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public static Field getField(Field f) {
+		f.setAccessible(true);
+		return f;
+	}
+	
+}
