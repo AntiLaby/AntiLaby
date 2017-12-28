35b581ee0138f7a449b331a4d2b8554b574ce2bf
heisluft <heisluft@icloud.com>
2017-12-28 22:15:39 +0100

    parse yaml: next try

--- src/main/java/com/github/antilaby/antilaby/lang/impl/LangFileParser.java
+++ src/main/java/com/github/antilaby/antilaby/lang/impl/LangFileParser.java
@@ -1,20 +1,5 @@
 package com.github.antilaby.antilaby.lang.impl;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.nio.charset.Charset;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.regex.Pattern;
-
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.configuration.file.YamlConstructor;
 import org.bukkit.configuration.file.YamlRepresenter;
@@ -23,22 +8,28 @@ import org.yaml.snakeyaml.DumperOptions.FlowStyle;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.representer.Representer;
 
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
 public class LangFileParser {
 	
 	private static final Pattern VERSION_PATTERN = Pattern.compile("((^|#)|.*\\s)version:\\s?\\d*\\.?\\d*\\.?\\d*$");
 	private static final Pattern REPLACER = Pattern.compile("^.*version:\\s?");
-
+	
 	public static LanguageVersion getVersion(File lFile) {
-		if (lFile == null || !lFile.exists()) throw new NullPointerException("File does not exist");
+		if(lFile == null || !lFile.exists()) throw new NullPointerException("File does not exist");
 		BufferedReader r = null;
 		try {
 			return getVersion((r = new BufferedReader(new FileReader(lFile))).readLine());
-		} catch (final Exception e) {
+		} catch(final Exception e) {
 			e.printStackTrace();
 		} finally {
 			try {
-				if (r != null) r.close();
-			} catch (final IOException e) {
+				if(r != null) r.close();
+			} catch(final IOException e) {
 				e.printStackTrace();
 			}
 		}
@@ -47,8 +38,8 @@ public class LangFileParser {
 	}
 	
 	public static LanguageVersion getVersion(String headerFrom) {
-		if (headerFrom == null || headerFrom.isEmpty()) return LanguageVersion.UNDEFINED;
-		if (VERSION_PATTERN.matcher(headerFrom).matches())
+		if(headerFrom == null || headerFrom.isEmpty()) return LanguageVersion.UNDEFINED;
+		if(VERSION_PATTERN.matcher(headerFrom).matches())
 			return LanguageVersion.getOrCreate(REPLACER.matcher(headerFrom).replaceAll(""));
 		return LanguageVersion.UNDEFINED;
 		
@@ -61,7 +52,7 @@ public class LangFileParser {
 	public static Map<String, String> parse(File f) {
 		try {
 			return parse(new FileInputStream(f), f.getName());
-		} catch (final FileNotFoundException e) {
+		} catch(final FileNotFoundException e) {
 			return null;
 		}
 	}
@@ -73,26 +64,29 @@ public class LangFileParser {
 	public static Map<String, String> parse(InputStream resource, String nameToUse) {
 		LanguageManager.LOG.info("Trying to load language resource: " + nameToUse);
 		final Map<String, String> result = new HashMap<>();
-		if (resource == null) return result;
+		if(resource == null) return result;
 		int line = 0;
 		try {
 			final BufferedReader r = new BufferedReader(new InputStreamReader(resource, Charset.forName("UTF-8")));
-			while (r.ready()) {
+			while(r.ready()) {
 				line++;
 				String ln = r.readLine();
 				ln = ln.trim();
-				if (!ln.startsWith("#")) if (!ln.contains("=")) LanguageManager.LOG
-						.warn("Could not parse line in resource " + nameToUse + " line " + line + ": " + ln);
+				if(!ln.startsWith("#")) if(!ln.contains("=")) LanguageManager.LOG
+						                                              .warn("Could not parse line in resource " +
+								                                                    nameToUse + " line " + line + ": "
+								                                                    + ln);
 				else {
 					final String[] s = ln.split("=", 2);
 					result.put(s[0].trim().replace(" ", ""), s[1].trim());
-
+					
 				}
 			}
 			r.close();
-		} catch (final Exception e) {
+		} catch(final Exception e) {
 			LanguageManager.LOG.error(
-					"Could not parse lang resource " + nameToUse + " (failed at line " + line + "): " + e.getMessage());
+					"Could not parse lang resource " + nameToUse + " (failed at line " + line + "): " + e.getMessage
+							                                                                                      ());
 			e.printStackTrace();
 		}
 		return result;
@@ -104,7 +98,7 @@ public class LangFileParser {
 		yamlOptions.setDefaultFlowStyle(FlowStyle.FLOW);
 		yamlRepresenter.setDefaultFlowStyle(FlowStyle.FLOW);
 		final String yamlString = new Yaml(new YamlConstructor(), yamlRepresenter, yamlOptions)
-				.dump(toDump.getValues(false));
+				                          .dump(toDump.getValues(false));
 		return new Yaml().<HashMap<String, String>>load(yamlString);
 	}
 	
@@ -117,5 +111,5 @@ public class LangFileParser {
 	public Map<String, String> parse() {
 		return parse(getClass().getClassLoader().getResourceAsStream(l.getName() + ".lang"), l.getName() + ".lang");
 	}
-
+	
 }
--- src/test/resources/com/github/antilaby/antilaby/TestClass.java
+++ src/test/resources/com/github/antilaby/antilaby/TestClass.java
@@ -1,11 +1,22 @@
 package com.github.antilaby.antilaby;
 
+import com.github.antilaby.antilaby.lang.impl.LangFileParser;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.junit.Test;
+import org.yaml.snakeyaml.Yaml;
 
-import com.github.antilaby.antilaby.lang.impl.LangFileParser;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TestClass {
+	
+	static String toCSV(String yamldata) {
+		Map<String, String> rt = new HashMap<>();
+		String currentKey = "";
+		Map<String, Object> yaml = Yaml.<Map<String, Object>>load(yamldata);
+		for(Map.Entry<String, Object> entry : yaml.entrySet()) System.out.println(entry);
+		return "";
+	}
 	
 	@Test
 	public void n() {
@@ -14,6 +25,7 @@ public class TestClass {
 		yaml.set("sdls.sssr", "%PLAYER% is an ideot");
 		yaml.set("sdls.sssr.a", "%PLAYER% is an ideot");
 		yaml.set("sdl.sssr", "%PLAYER% is an ideot");
+		toCSV(yaml);
 		LangFileParser.migrateYamlToLang(yaml);
 	}
 }
