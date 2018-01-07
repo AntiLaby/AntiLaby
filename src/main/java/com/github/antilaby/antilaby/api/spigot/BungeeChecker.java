5d0183e196e1fb3f725e839ae7d7f16e26eeaa57
heisluft <heisluft@icloud.com>
2018-01-07 18:47:49 +0100

    fixed JDoc

--- src/main/java/com/github/antilaby/antilaby/api/spigot/BungeeChecker.java
+++ src/main/java/com/github/antilaby/antilaby/api/spigot/BungeeChecker.java
@@ -2,26 +2,21 @@ package com.github.antilaby.antilaby.api.spigot;
 
 import java.lang.reflect.Field;
 
+/**
+ * Check if a server uses BungeeCord
+ *
+ * @author NathanNr
+ */
 public class BungeeChecker {
-
-	/**
-	 * Check if a server uses BungeeCord
-	 * 
-	 * @author NathanNr
-	 */
 
 	private boolean isBungeecord;
 
 	public BungeeChecker() {
-		init();
-	}
-
-	public void init() {
 		try {
 			Field field = Class.forName("org.spigotmc.SpigotConfig").getDeclaredField("bungee");
 			field.setAccessible(true);
 			this.isBungeecord = field.getBoolean(null);
-		} catch (Exception e) {
+		} catch(Exception e) {
 			this.isBungeecord = false;
 		}
 	}
