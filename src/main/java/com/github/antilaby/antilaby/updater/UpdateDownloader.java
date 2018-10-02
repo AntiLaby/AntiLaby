eba7a40bbf4cf4d583ca6dedc9aff7bdf42c405a
Nathan <17128682+NathanNr@users.noreply.github.com>
2018-10-02 19:25:58 +0200

    Created the update downloader (#18)

--- src/main/java/com/github/antilaby/antilaby/updater/UpdateDownloader.java
+++ src/main/java/com/github/antilaby/antilaby/updater/UpdateDownloader.java
@@ -1,8 +1,21 @@
 package com.github.antilaby.antilaby.updater;
 
-import java.io.IOException;
+import com.github.antilaby.antilaby.log.Logger;
+import com.github.antilaby.antilaby.main.AntiLaby;
+import com.github.antilaby.antilaby.util.IOUtils;
 
+import java.net.URL;
+
+import java.io.*;
+
+/**
+ * Downloads the updated file
+ *
+ * @author NathanNr
+ */
 public class UpdateDownloader {
+
+	private static final Logger LOGGER = new Logger("UpdateDownloader");
 
 	private final UpdateInformation updateInformation;
 	private final String temporaryFileLocation;
@@ -12,9 +25,27 @@ public class UpdateDownloader {
 		this.temporaryFileLocation = temporaryFileLocation;
 	}
 
+	/**
+	 * Download the updated file
+	 *
+	 * @throws IOException
+	 */
 	public void download() throws IOException {
-
-		// TODO
+		LOGGER.debug("Downloading update from download server...");
+		final InputStream inputStream = new BufferedInputStream(new URL(updateInformation.getDownloadUrl()).openConnection().getInputStream());
+		final OutputStream outputStream = new BufferedOutputStream(new FileOutputStream(temporaryFileLocation));
+		IOUtils.copyStream(inputStream, outputStream);
+		outputStream.close();
+		final File tempFile = new File(temporaryFileLocation);
+		final long newlength = tempFile.length();
+		LOGGER.debug("Installing update...");
+		final FileInputStream fileInputStream = new FileInputStream(tempFile);
+		final OutputStream outputStream1 = new BufferedOutputStream(new FileOutputStream(AntiLaby.getInstance().getFile()));
+		IOUtils.copyStream(fileInputStream, outputStream1);
+		fileInputStream.close();
+		outputStream1.close();
+		tempFile.delete();
+		LOGGER.debug("Done!");
 	}
 
 	public String getTemporaryFileLocation() {
